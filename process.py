11a856f0a58f8f979a08d979d624375b03eee913
Piotr Tutak <p.tutak@outlook.com>
2020-01-23 23:14:55 +0100

    testing

--- process.py
+++ process.py
@@ -48,6 +48,32 @@ def compute_cooling_speed(dataframe):
     dataframe['cooling_rate'] = - dataframe.diff(axis=0)['avg_temp']
 
 
+def add_chart(writer, sheet_name, shape):
+    workbook = writer.book
+    worksheet = writer.sheets[sheet_name]
+    chart = workbook.add_chart({'type': 'line'})
+    chart.add_series({
+        'categories': [sheet_name, 1, 0, shape[0] + 1, 0],
+        'values': [sheet_name, 1, 4, shape[0] + 1, 4]
+    })
+    chart.set_x_axis({'name': 'Time'})
+    chart.set_y_axis({'name': 'Average temperature', 'major_gridlines': {'visible': True}})
+    chart.set_legend({'position': 'none'})
+
+    worksheet.insert_chart('H2', chart)
+
+    chart = workbook.add_chart({'type': 'line'})
+    chart.add_series({
+        'categories': [sheet_name, 1, 4, shape[0] + 1, 4],
+        'values': [sheet_name, 1, 5, shape[0] + 1, 5]
+    })
+    chart.set_x_axis({'name': 'Average temperature'})
+    chart.set_y_axis({'name': 'Cooling rate', 'major_gridlines': {'visible': True}})
+    chart.set_legend({'position': 'none'})
+
+    worksheet.insert_chart('H22', chart)
+
+
 if __name__ == '__main__':
     args = parse_args()
     file_dict = {}
@@ -60,10 +86,15 @@ if __name__ == '__main__':
         compute_mean(dataframe)
         compute_cooling_speed(dataframe)
 
-    with ExcelWriter(args.output_file) as writer:
+    output_file = args.output_file
+    if not output_file.endswith('.xlsx'):
+        output_file += '.xlsx'
+
+    with ExcelWriter(args.output_file, engine='xlsxwriter') as writer:
         for filename, dataframe in file_dict.items():
+            sheet_name = os.path.basename(filename).rstrip('.csv')
             dataframe.to_excel(
                 excel_writer=writer,
-                sheet_name=os.path.basename(filename).rstrip('.csv'))
-
-    pprint(file_dict)
+                sheet_name=sheet_name,
+                index_label='time')
+            add_chart(writer, sheet_name, dataframe.shape)
